7171175b3841b80edd6cf75b7c01145f815e8165
CleytonSouzaAlmeida <cleytoneaa@gmail.com>
2020-05-08 23:06:40 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 
 package application;
 
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 
 public class Program {
     public static void main(String[] args) {
         
         Department novo = new Department(1, "books");
-        System.out.println(novo);
+        
+        Seller vendedor = new Seller(21, "Joao", "joao@gmail.com", new Date(), 10000.00, novo);
+        System.out.println(vendedor);
     }
 }
