9d53f4517ec7b01827cad76bd0eda5c2f4cfe251
CleytonSouzaAlmeida <cleytoneaa@gmail.com>
2020-05-09 11:33:53 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@
 package application;
 
 import java.util.Date;
+import model.DAO.DaoFactory;
+import model.DAO.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
     public static void main(String[] args) {
         
         Department novo = new Department(1, "books");
+        
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         
         Seller vendedor = new Seller(21, "Joao", "joao@gmail.com", new Date(), 10000.00, novo);
         System.out.println(vendedor);
---
+++ src/model/DAO/impl/SellerDaoJDBC.java
@@ -0,0 +1,36 @@
+
+package model.DAO.impl;
+
+import java.util.List;
+import model.DAO.SellerDao;
+import model.entities.Seller;
+
+
+public class SellerDaoJDBC  implements SellerDao{
+
+    @Override
+    public void insert(Seller obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Seller obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteById(Integer Id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Seller findById(Integer Id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
